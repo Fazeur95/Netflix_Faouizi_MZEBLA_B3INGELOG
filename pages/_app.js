6aef14742fbf15b972d396d9fad9c9b8b221c5d5
Faouizi MZEBLA <faouizi.mzebla@ynov.com>
2022-04-09 17:17:05 +0200

    Ajouts de fonctions
    Adding functionality

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,8 @@
 import "../styles/globals.css";
 import "../styles/Row.scss";
-<<<<<<< Updated upstream
-import "../styles/Nav.scss";
-import "../styles/QuickView.scss";
-import "../styles/Video.scss";
-
-=======
 import "../styles/Video.scss";
 import "../styles/QuickView.scss";
 import "../styles/Nav.scss";
->>>>>>> Stashed changes
 import { FirebaseContext } from "../context/FirbaseContext";
 import firebase from "../lib/firebase.prod";
 
